745409fbd260c305ae4cec84750430cfd2a8d016
Live session user <nicoustie@gmail.com>
2016-12-13 15:22:56 +0000

    Fix demo module

--- alkotool/manifests/init.pp
+++ alkotool/manifests/init.pp
@@ -22,13 +22,13 @@ class alkotool {
 	}
 
 	file { '/etc/nginx/nginx.conf':
-		content => template('nginx/nginx.conf'),
+		content => template('alkotool/nginx.conf'),
 		notify => Service['nginx'],
 		require => Package['nginx'],
 	}
 
 	file { '/etc/nginx/sites-available/default':
-		content => template('nginx/default'),
+		content => template('alkotool/default'),
 		notify => Service['nginx'],		
 		require => Package['nginx'],
 	}
@@ -46,7 +46,7 @@ class alkotool {
 	}
 
 	file { '/var/lib/tomcat8/conf/server.xml':
-		content		=> template('tomcat8/server.xml'),
+		content		=> template('alkotool/server.xml'),
 		notify		=> Service['tomcat8'],
 		require		=> Package['tomcat8'],
 	}
@@ -59,7 +59,7 @@ class alkotool {
 
 	# Download and deploy Alkotool .war to Tomcat
 	exec { 'alkodeploy':
-		path => ['/usr/bin', '/usr/sbin',],
+		path => "/bin/:/usr/bin/:/sbin/:/usr/sbin/",
 		command => "wget https://nicou.me/alkotool.war -P /var/lib/tomcat8/webapps/",
 		creates => "/var/lib/tomcat8/webapps/alktool.war",
 		require => Service['tomcat8'],
@@ -67,8 +67,8 @@ class alkotool {
 
 	# Download and deploy Alkotool database to MariaDB
 	exec { 'alkodbdeploy':
-		path => ['/usr/bin', '/usr/sbin',],
-		command => "mysql -e \"CREATE DATABASE juomakanta;\" && mysql -e \"CREATE USER 'alkotool'@'localhost' IDENTIFIED BY 'AwDvklPURO2k7Ql';\" && mysql -e \"GRANT ALL PRIVILEGES ON juomakanta.* TO 'alkotool'@'localhost';\" && wget https://nicou.me/alkodb.sql -P /tmp/ && mysql juomakanta < /tmp/alkodb.sql && rm /tmp/alkodb.sql", 
+		path => "/bin/:/usr/bin/:/sbin/:/usr/sbin/",
+		command => "mysql -e \"CREATE DATABASE juomakanta;\" && mysql -Bse \"CREATE USER 'alkotool'@'localhost' IDENTIFIED BY 'AwDvklPURO2k7Ql';\" && mysql -Bse \"GRANT ALL PRIVILEGES ON juomakanta.* TO 'alkotool'@'localhost';\" && wget https://nicou.me/alkodb.sql -P /tmp/ && mysql juomakanta < /tmp/alkodb.sql && rm /tmp/alkodb.sql", 
 		unless => "mysqlshow juomakanta",
 		require => Service["mysql"],
 	}
